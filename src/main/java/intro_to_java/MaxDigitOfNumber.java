44c593f0f9e7d414f4f330719f4508c6afc8f615
paula <iuliapaula@yahoo.com>
2019-11-21 20:43:57 +0200

    first commit

---
+++ src/main/java/intro_to_java/MaxDigitOfNumber.java
@@ -0,0 +1,12 @@
+package intro_to_java;
+
+import java.util.Scanner;
+
+public class MaxDigitOfNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please provide a number:");
+        int number = scanner.nextInt()
+
+    }
+}
