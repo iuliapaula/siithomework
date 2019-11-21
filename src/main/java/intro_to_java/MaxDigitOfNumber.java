e551b9fc0d66d2c7b955b42696aa684145a13cb3
paula <iuliapaula@yahoo.com>
2019-11-21 21:29:47 +0200

    max digit

--- src/main/java/intro_to_java/MaxDigitOfNumber.java
+++ src/main/java/intro_to_java/MaxDigitOfNumber.java
@@ -6,7 +6,14 @@ public class MaxDigitOfNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please provide a number:");
-        int number = scanner.nextInt()
-
+        int number = scanner.nextInt();
+        int maximumDigit = 0;
+        while(number > 0)
+        {
+            if(number % 10 > maximumDigit)
+                maximumDigit = number % 10;
+            number = number / 10;
+        }
+        System.out.println("Maximum digit of number is: " + maximumDigit);
     }
 }
